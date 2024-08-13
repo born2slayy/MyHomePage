12d3d84171ffad1d9f5d58d639c20859414fbe09
born2slayy77 <born2slayy77@gmail.com>
2024-08-14 01:21:16 +0900

    build

--- src/components/component/my-home-page.tsx
+++ src/components/component/my-home-page.tsx
@@ -34,7 +34,7 @@ export function MyHomePage() {
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link href="#" className="flex items-center justify-center" prefetch={false}>
           <MountainIcon className="size-6" />
-          <span className="sr-only">Hyeji Chang's Portfolio</span>
+          <span className="sr-only">Hyeji Chang&apos;s Portfolio</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
@@ -281,13 +281,17 @@ export function MyHomePage() {
   )
 }
 
-function GithubIcon(props) {
+interface GithubIconProps {
+  width?: number;  // 옵션 속성 (기본값 24)
+  height?: number; // 옵션 속성 (기본값 24)
+  className?: string; // 이전 섹션에서 추가한 className 속성
+}
+
+function GithubIcon(props: GithubIconProps) {
   return (
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -298,17 +302,19 @@ function GithubIcon(props) {
       <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
       <path d="M9 18c-4.51 2-5-2-7-2" />
     </svg>
-  )
+  );
 }
 
-
-function LinkedinIcon(props) {
+interface LinkedinIconProps {
+  width?: number;  // 옵션 속성 (기본값 24)
+  height?: number; // 옵션 속성 (기본값 24)
+  className?: string; // 이전 섹션에서 추가한 className 속성
+}
+function LinkedinIcon(props: LinkedinIconProps) {
   return (
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -323,14 +329,17 @@ function LinkedinIcon(props) {
   )
 }
 
+interface MountainIconProps {
+  width?: number;  // 옵션 속성 (기본값 24)
+  height?: number; // 옵션 속성 (기본값 24)
+  className?: string; // 이전 섹션에서 추가한 className 속성
+}
 
-function MountainIcon(props) {
+function MountainIcon(props: MountainIconProps) {
   return (
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
